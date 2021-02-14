5973bd715f5fe9af36778f3c3731ad07ce541668
Andre Baptista <albs_br@yahoo.com.br>
2021-02-14 13:17:51 -0300

    v.0.4.0

---
+++ Graphics/Tiles/Colors.s
@@ -0,0 +1,10 @@
+TileColors_SmallBricks_Start:
+        db  0x00
+        db  0x07
+        db  0x05
+        db  0x05
+        db  0x00
+        db  0x05
+        db  0x04
+        db  0x04
+TileColors_SmallBricks_End:
---
+++ Graphics/Tiles/Patterns.s
@@ -0,0 +1,81 @@
+TilePatterns_SmallBricks_Start:
+;Frame # 0
+        db      00000000 b
+        db      11111101 b
+        db      11111101 b
+        db      11111101 b
+        db      00000000 b
+        db      11011101 b
+        db      11011101 b
+        db      11011101 b
+
+;Frame # 1
+        db      00000000 b
+        db      11111011 b
+        db      11111011 b
+        db      11111011 b
+        db      00000000 b
+        db      10111011 b
+        db      10111011 b
+        db      10111011 b
+
+;Frame # 2
+        db      00000000 b
+        db      11110111 b
+        db      11110111 b
+        db      11110111 b
+        db      00000000 b
+        db      01110111 b
+        db      01110111 b
+        db      01110111 b
+
+;Frame # 3
+        db      00000000 b
+        db      11101111 b
+        db      11101110 b
+        db      11101111 b
+        db      00000000 b
+        db      11101110 b
+        db      11101110 b
+        db      11101110 b
+
+;Frame # 4
+        db      00000000 b
+        db      11011111 b
+        db      11011101 b
+        db      11011111 b
+        db      00000000 b
+        db      11011101 b
+        db      11011101 b
+        db      11011101 b
+
+;Frame # 5
+        db      00000000 b
+        db      10111111 b
+        db      10111011 b
+        db      10111111 b
+        db      00000000 b
+        db      10111011 b
+        db      10111011 b
+        db      10111011 b
+
+;Frame # 6
+        db      00000000 b
+        db      01111111 b
+        db      01110111 b
+        db      01111111 b
+        db      00000000 b
+        db      01110111 b
+        db      01110111 b
+        db      01110111 b
+
+;Frame # 7
+        db      00000000 b
+        db      11111110 b
+        db      11101110 b
+        db      11111110 b
+        db      00000000 b
+        db      11101111 b
+        db      11101111 b
+        db      11101111 b
+TilePatterns_SmallBricks_End:
