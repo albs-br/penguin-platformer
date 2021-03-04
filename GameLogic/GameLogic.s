967cfcae34c6b52223d126f3e4c2923625c0e420
Andre Baptista <albs_br@yahoo.com.br>
2021-03-03 23:47:23 -0300

    v.0.32.0

--- GameLogic/GameLogic.s
+++ GameLogic/GameLogic.s
@@ -6,57 +6,14 @@ GameLogic:
     ; check if player is at empty space
     ; formula: COL_NUMBER + ((TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * LINE_NUMBER)     (only first 4 rows)
     
-
-    ; b = LINE_NUMBER:
-    ld      a, (Test_Sprite_Y)
-    srl     a       ; divide by 8
-    srl     a
-    srl     a
-    ld      c, a
-    and     0000 0011 b         ; mask to get only lower 2 bits of line number and make it a line number 0-3
-    ld      b, a
-    ld      a, c
-    and     0001 1100 b         ; mask to get the page number (0-5)
-    srl     a                   ; align to right
-    srl     a
-    inc     a                   ; pages are numbered 1-6, not 0-5
-    ld      c, a
-
-
-
-    ; hl = (TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * LINE_NUMBER
-    ld      hl, 0
-    jp      z, .continue
-    ld      de, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8
-.loopMulti:
-    add     hl, de
-    djnz    .loopMulti
-.continue:
-
-    ; de = COL_NUMBER:
     ld      a, (Test_Sprite_X)
-    srl     a       ; divide by 8
-    srl     a
-    srl     a
-    ld      d, 0
-    ld      e, a
-
-    ; de = COL_NUMBER + ((TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * LINE_NUMBER)
-    add     hl, de
-    ex      de, hl
-
-
-    ; set MegaROM page
-    ld      a, c
-    ld	    (Seg_P8000_SW), a
-
-    ld      hl, (BgAddrIndex)
-    ;ld      de, 2 + (TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * 2
-    add     hl, de
-    ld      a, (hl)
-    or      a
+    ld      h, a
+    ld      a, (Test_Sprite_Y)
+    ld      l, a
+    call    CheckBackGround
     call    z, .playerAtEmptySpace
     call    nz, .playerAtOccupiedSpace
+
 
     ld      (Test_Sprite_Color), a
 
@@ -132,4 +89,69 @@ UpdateSpriteAttrTableBuffer:
     ld      a, COLOR_WHITE
     ld      (hl), a
 
+    ret
+
+
+; Checks if a given XY coord is over a blank tile
+;   Inputs:
+;       H: x coordinate (0-255)
+;       L: y coordinate (0-191)
+;   Output:
+;       z: is at empty space
+;       nz: is not at empty space
+CheckBackGround:
+    push    hl                  ; save h (x coord)
+
+    ; b = LINE_NUMBER:
+    ld      a, l
+    srl     a       ; divide by 8
+    srl     a
+    srl     a
+    ld      c, a
+    and     0000 0011 b         ; mask to get only lower 2 bits of line number and make it a line number 0-3
+    ld      b, a
+    
+    ; c = MegaROM Page number
+    ld      a, c
+    and     0001 1100 b         ; mask to get the page number (0-5)
+    srl     a                   ; align to right
+    srl     a
+    inc     a                   ; pages are numbered 1-6, not 0-5
+    ld      c, a
+
+
+
+    ; hl = (TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * LINE_NUMBER
+    ld      hl, 0
+    jp      z, .continue
+    ld      de, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8
+.loopMulti:
+    add     hl, de
+    djnz    .loopMulti
+.continue:
+
+    ; de = COL_NUMBER:
+    pop     af                  ; restore h (x coord)
+    ;ld      a, d
+    srl     a                   ; divide by 8
+    srl     a
+    srl     a
+    ld      d, 0
+    ld      e, a
+
+    ; de = COL_NUMBER + ((TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * LINE_NUMBER)
+    add     hl, de
+    ex      de, hl
+
+
+    ; set MegaROM page
+    ld      a, c
+    ld	    (Seg_P8000_SW), a
+
+    ld      hl, (BgAddrIndex)
+    ;ld      de, 2 + (TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8) * 2
+    add     hl, de
+    ld      a, (hl)
+    or      a
+    
     ret
