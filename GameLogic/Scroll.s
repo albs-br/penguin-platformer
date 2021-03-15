0f2a54cc4f871ca7dd6ab5f02c80fdf6d43af914
Andre Baptista <albs_br@yahoo.com.br>
2021-03-15 19:14:02 -0300

    v.0.45.1

--- GameLogic/Scroll.s
+++ GameLogic/Scroll.s
@@ -1,79 +1,3 @@
-; Scroll_old:
-
-;     ; search names table buffer looking for tiles 
-;     ; other than 0
-;     ld      hl, NamesTableBuffer + 512
-;     xor     a                           ; same as ld a, 0
-;     ld      b, 0                        ; counter
-; .loop:
-;     or      (hl)
-;     jp      z, .next
-
-; .animate:
-
-;     inc     (hl)
-
-; .next:
-
-;     inc     hl
-;     djnz    .loop
-    
-;     ; at each 8 pixel fine scrolls, do a tile scroll
-;     ld      a, (BgScrollRegister)
-;     inc     a
-;     cp      8
-;     call    z, ScrollTiles
-
-;     ld      (BgScrollRegister), a
-
-;     ; call    UpdateNamesTable_BackGround
-;     ;call    UpdateNamesTable
-
-;     ret
-
-; ScrollTiles:
-
-; ; test only last line
-
-;     ; scroll all tiles
-; 	; ld	    hl, (NextBgLineAddr)
-; 	ld	    hl, TileMap_LevelTest_Start + SCREEN_HEIGHT_IN_TILES - 1    ; Origin
-;     ld      de, NamesTableBuffer + (SCREEN_WIDTH_IN_TILES * 23)         ; Destiny
-;     ld      b, SCREEN_WIDTH_IN_TILES - 1
-; .loop:
-;     ld      a, (hl)
-;     ld      (de), a
-;     push    bc
-;     ld      bc, SCREEN_HEIGHT_IN_TILES
-;     add     hl, bc
-;     pop     bc
-;     inc     de
-; ; .loopEnternal:
-; ;     jp .loopEnternal
-;     djnz    .loop    
-
-;     ;inc     de
-;     ld      a, 1
-;     ld      (de), a ; load tile in last column
-
-
-;     ; load next bg line
-;     ; ld	    hl, (NextBgLineAddr)
-; 	; ld	    bc, 32                                                  ; Block length
-;     ; and     a                                                       ; Clear C flag
-;     ; sbc     hl, bc
-;     ; ld	    (NextBgLineAddr), hl
-;     ; ld      de, NamesTableBuffer + SCREEN_WIDTH_IN_TILES            ; Destiny
-;     ; ; ldir                                                        ; Copy BC number of bytes from HL to DE
-;     ; ; 32 unrolled ldi's (160 less cycles). Made with MSXUtilities
-;     ; ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi ldi 
-
-;     ; clear BgScrollRegister when exit subroutine
-;     xor     a                                                       ; same as ld a, 0 but faster
-
-;     ret
-
-
 NextPage:
     inc     e   ; next page
     ld      a, e
@@ -91,7 +15,6 @@ ScrollRight:
     ; check if scroll is at limit
     ld      hl, (BgCurrentIndex)
     ld      de, 8 * (TILE_MAP_WIDTH_IN_8X8_COLUMNS - SCREEN_WIDTH_IN_TILES)
-    ; ld      de, 8 * (256 - SCREEN_WIDTH_IN_TILES)
     call    BIOS_DCOMPR                 ; Compares HL with DE. Zero flag set if HL and DE are equal. Carry flag set if HL is less than DE.
     ret     nc                          ; hl >= de
 
@@ -114,76 +37,7 @@ ScrollRight:
     inc     hl
 .normalSpeed:
     
-    ld      (BgCurrentIndex), hl
-
-    ; Sets the VRAM pointer (destiny)
-	ld	    hl, NamesTable
-	call    BIOS_SETWRT
-
-            ld	    hl, (BgAddrIndex)
-            
-            ; set MegaROM initial page
-            ld      e, 1                    
-            ld      a, e
-	        ld	    (Seg_P8000_SW), a
-
-            ; First n lines with unroled OUTI's during Vblank
-            ld      d, SCROLL_TILE_LINES_DURING_VBLANK
-        .loopLines1:
-            ; Set the source pointer in RAM
-            push    hl
-
-            ld	    a, (BIOS_VDP_DW)
-            ld	    c, a
-            ; 32 Unrolled OUTIs (use only during v-blank)
-            OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI OUTI 
-
-            ; Update bgIndex to next line
-            ; BgIndex += 128 * 8
-            pop     hl
-            ld      bc, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8
-            add     hl, bc
-
-            ; check if line is multiple of 4 (go to next page)
-            ld      a, d
-            dec     a
-            and     0000 0011 b
-            call    z, NextPage
-
-            dec     d
-            jp      nz, .loopLines1
-
-
-
-            ; last 24-n lines with the slower OUTI inside loop after Vblank
-            ld      d, SCROLL_TILE_LINES_AFTER_VBLANK
-        .loopLines2:
-            ; Set the source pointer in RAM
-            push    hl
-
-            ld	    a, (BIOS_VDP_DW)
-            ld	    c, a
-
-                    ld      b, 32
-                .loopOUTI:
-                    outi
-                    jp	    nz, .loopOUTI
-
-            ; Update bgIndex to next line
-            ; BgIndex += 128 * 8
-            pop     hl
-            ld      bc, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 8
-            add     hl, bc
-
-            ; check if line is multiple of 4 (go to next page)
-            ld      a, d
-            dec     a
-            and     0000 0011 b
-            call    z, NextPage
-
-            dec     d
-            jp      nz, .loopLines2
-
+    call    DrawBackground
 
 ; TODO: 1x or 2x speed here
 	ld	    hl, (BgAddrIndex)
@@ -263,6 +117,49 @@ ScrollLeft:
 
 
     dec     hl
+
+    call    DrawBackground
+
+	ld	    hl, (BgAddrIndex)
+    ; hl = hl - TileMapSizeInColumns
+    ld      de, - TILE_MAP_WIDTH_IN_8X8_COLUMNS
+    ;or      a                               ; clear carry flag
+    ; sbc     hl, de
+    add     hl, de
+    ld      (BgAddrIndex), hl
+
+    ; dec FrameIndex
+    ld      de, FrameIndex
+    ld      a, (de)
+    dec     a
+
+    ; if (FrameIndex) == -1 {
+    ;   FrameIndex = 7;
+    ;   (BgIndexFirstFrame)--;        
+    ;   (BgIndex) = (BgIndexFirstFrame) + (TileMapSizeInColumns)*7;
+    ; }
+    cp      -1
+    jp      nz, .notMinus1
+
+;  
+    ;FrameIndex == -1:
+    ld      hl, (BgAddrIndexFirstFrame)
+    dec     hl
+    ld      (BgAddrIndexFirstFrame), hl
+    
+    ld      bc, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 7
+    add     hl, bc
+    ld      (BgAddrIndex), hl
+
+    ld      a, 7
+.notMinus1:
+    ld      (de), a
+    
+    ret
+
+
+
+DrawBackground:
     ld      (BgCurrentIndex), hl
 
     ; Sets the VRAM pointer (destiny)
@@ -333,41 +230,4 @@ ScrollLeft:
             dec     d
             jp      nz, .loopLines2
 
-
-
-	ld	    hl, (BgAddrIndex)
-    ; hl = hl - TileMapSizeInColumns
-    ld      de, - TILE_MAP_WIDTH_IN_8X8_COLUMNS
-    ;or      a                               ; clear carry flag
-    ; sbc     hl, de
-    add     hl, de
-    ld      (BgAddrIndex), hl
-
-    ; dec FrameIndex
-    ld      de, FrameIndex
-    ld      a, (de)
-    dec     a
-
-    ; if (FrameIndex) == -1 {
-    ;   FrameIndex = 7;
-    ;   (BgIndexFirstFrame)--;        
-    ;   (BgIndex) = (BgIndexFirstFrame) + (TileMapSizeInColumns)*7;
-    ; }
-    cp      -1
-    jp      nz, .notMinus1
-
-;  
-    ;FrameIndex == -1:
-    ld      hl, (BgAddrIndexFirstFrame)
-    dec     hl
-    ld      (BgAddrIndexFirstFrame), hl
-    
-    ld      bc, TILE_MAP_WIDTH_IN_8X8_COLUMNS * 7
-    add     hl, bc
-    ld      (BgAddrIndex), hl
-
-    ld      a, 7
-.notMinus1:
-    ld      (de), a
-    
     ret
