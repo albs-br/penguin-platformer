f70aa7640d72d502b05e2f06f984bf2906dca0e5
Andre Baptista <albs_br@yahoo.com.br>
2021-04-24 23:55:51 -0300

    v.0.50.4

--- GameLogic/GameConstants.s
+++ GameLogic/GameConstants.s
@@ -18,24 +18,26 @@ PENGUIN_RIGHT_STANDING:                 equ 1 * 4
 PENGUIN_RIGHT_WALKING_1:                equ 3 * 4
 PENGUIN_RIGHT_WALKING_2:                equ 5 * 4
 PENGUIN_RIGHT_WALKING_3:                equ 7 * 4
-PENGUIN_RIGHT_WALKING_LAST_FRAME:       equ PENGUIN_RIGHT_WALKING_3
-PENGUIN_LEFT_STANDING:                  equ 9 * 4
-PENGUIN_LEFT_WALKING_1:                 equ 11 * 4
-PENGUIN_LEFT_WALKING_2:                 equ 13 * 4
-PENGUIN_LEFT_WALKING_3:                 equ 15 * 4
+PENGUIN_RIGHT_WALKING_4:                equ 9 * 4
+PENGUIN_RIGHT_WALKING_5:                equ 11 * 4
+PENGUIN_RIGHT_WALKING_LAST_FRAME:       equ PENGUIN_RIGHT_WALKING_5
+PENGUIN_LEFT_STANDING:                  equ 13 * 4
+PENGUIN_LEFT_WALKING_1:                 equ 15 * 4
+PENGUIN_LEFT_WALKING_2:                 equ 17 * 4
+PENGUIN_LEFT_WALKING_3:                 equ 19 * 4
 PENGUIN_LEFT_WALKING_LAST_FRAME:        equ PENGUIN_LEFT_WALKING_3
 
 ; These frames must be the last of the penguin frames
-PENGUIN_TURNING_LEFT_TO_RIGHT_0:                 equ 17 * 4
-PENGUIN_TURNING_LEFT_TO_RIGHT_1:                 equ 19 * 4
-PENGUIN_TURNING_LEFT_TO_RIGHT_2:                 equ 21 * 4
-PENGUIN_TURNING_LEFT_TO_RIGHT_3:                 equ 23 * 4
+PENGUIN_TURNING_LEFT_TO_RIGHT_0:                 equ 21 * 4
+PENGUIN_TURNING_LEFT_TO_RIGHT_1:                 equ 23 * 4
+PENGUIN_TURNING_LEFT_TO_RIGHT_2:                 equ 25 * 4
+PENGUIN_TURNING_LEFT_TO_RIGHT_3:                 equ 27 * 4
 PENGUIN_TURNING_LEFT_TO_RIGHT_LAST_FRAME:        equ PENGUIN_TURNING_LEFT_TO_RIGHT_3
 
 
 ; Sprite pattern numbers for sparkles animation frames:
-SPARKLES_FIRST_FRAME:       equ 25 * 4
-SPARKLES_LAST_FRAME:        equ 33 * 4
+SPARKLES_FIRST_FRAME:       equ 29 * 4
+SPARKLES_LAST_FRAME:        equ 37 * 4
 
 
 ; Keyboard constants to current key(s) pressed
