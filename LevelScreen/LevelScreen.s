c71b23a815a31ac45195ed87cc2aa1a99db466b8
Andre Baptista <albs_br@yahoo.com.br>
2022-06-14 13:37:48 -0300

    v.0.74.3

--- LevelScreen/LevelScreen.s
+++ LevelScreen/LevelScreen.s
@@ -4,6 +4,8 @@ LevelScreen:
 
     call    InitVram
 
+    call    BIOS_ENASCR
+
     xor     a
     ld      (LevelScreen_Counter), a
 
@@ -19,37 +21,49 @@ LevelScreen:
 
     ld      a, (LevelScreen_Counter)
     
-    cp      4               ; if(a >=0 && a<= 3)
-    jp      c, .frame_0
+    cp      0
+    call    z, .frame_0
     
-    cp      8               ; if(a >=4 && a<= 7)
-    ; jp      c, .frame_1
+    cp      4
+    call    z, .frame_1
     
-    cp      12              ; if(a >=8 && a<= 11)
-    ; jp      c, .frame_2
+    ; cp      8
+    ; call    z, .frame_2
     
-    cp      16              ; if(a >=12 && a<= 15)
-    ; jp      c, .frame_3
+    cp      12              ; if(a >=12 && a<= 15)
+    ; call    z, .frame_3
     
-    cp      20              ; if(a >=16 && a<= 19)
-    ; jp      c, .frame_4
+    cp      16              ; if(a >=16 && a<= 19)
+    ; call    z, .frame_4
     
-    cp      24              ; if(a >=20 && a<= 23)
-    ; jp      c, .frame_5
+    cp      20              ; if(a >=20 && a<= 23)
+    ; call    z, .frame_5
     
-.eternalLoop:
-    jp      .eternalLoop
+    ; cp      255
+    ; jp    z, .end
+
+
+    ld      hl, LevelScreen_Counter
+    inc     (hl)
+
+    jp      .levelScreenLoop
+
 
 
     ; -------------------- frame 0:  Sprite normal       (pixel size 1x1)
 .frame_0:
-	call 	BIOS_DISSCR
+	;call 	BIOS_DISSCR
 
-    ; Create Sprites
-	ld		hl, SpritePatterns        				; RAM address (source)
-	ld		de, SpritePatternTable					; VRAM address (destiny)
-	ld		bc, 3*16 ; SpritePatterns.size					; Block length
-    call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
+    ; set sprites normal size
+	ld		c, 1	               		; VDP Register Number (0..27, 32..46)
+	ld		b, 1110 0010 b   	        ; Data To Write
+    call 	BIOS_WRTVDP        		    ; 
+
+    ; ; Create Sprites
+	; ld		hl, SpritePatterns        				; RAM address (source)
+	; ld		de, SpritePatternTable					; VRAM address (destiny)
+	; ld		bc, 3*16 ; SpritePatterns.size					; Block length
+    ; call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
 
     ; Load Sprite Attributes
 	ld		hl, SPRITE_ATT_TABLE_FRAME_0			; RAM address (source)
@@ -57,20 +71,28 @@ LevelScreen:
 	ld		bc, SPRITE_ATT_TABLE_FRAME_0.size		; Block length
     call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
 
-	call 	BIOS_ENASCR
-
-.eternalLoop1:
-    jp      .eternalLoop1
+	;call 	BIOS_ENASCR
 
     ret
 
-SPRITE_ATT_TABLE_FRAME_0:
-    ; Y, X, pat, color
-    db (192/2)-8, (256/2)-8, 1*4, 4
-    db (192/2)-8, (256/2)-8, 2*4, 15
-.size: equ $ - SPRITE_ATT_TABLE_FRAME_0
+    ; -------------------- frame 1:  Sprite magnified    (pixel size 2x2)
+.frame_1:
+    ; set sprites magnified
+    ;   bit 0: MAG enlarges the sprites when 1 is written. (0 by default)
+	ld		c, 1	               		; VDP Register Number (0..27, 32..46)
+	ld		b, 1110 0011 b   	        ; Data To Write
+    call 	BIOS_WRTVDP        		    ; 
 
-    ; frame 1:  Sprite magnified    (pixel size 2x2)
+    ; Load Sprite Attributes
+	ld		hl, SPRITE_ATT_TABLE_FRAME_1			; RAM address (source)
+	ld		de, SpriteAttrTable					    ; VRAM address (destiny)
+	ld		bc, SPRITE_ATT_TABLE_FRAME_1.size		; Block length
+    call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
+
+    ret
+
+
+
     ; frame 2:  Tiles               (pixel size 4x4)
     ; frame 3:  Tiles               (pixel size 8x8)
     ; frame 4:  Tiles               (pixel size 16x16)
@@ -78,7 +100,18 @@ SPRITE_ATT_TABLE_FRAME_0:
 
     ; 6 frames x 4 = 24 frames (less than half second)
 
-    ld      hl, LevelScreen_Counter
-    inc     (hl)
 
-    ret
+
+
+; --------------- Data
+SPRITE_ATT_TABLE_FRAME_0:
+    ; Y, X, pat, color
+    db (192/2)-8, (256/2)-8, 1*4, 4
+    db (192/2)-8, (256/2)-8, 2*4, 15
+.size: equ $ - SPRITE_ATT_TABLE_FRAME_0
+
+SPRITE_ATT_TABLE_FRAME_1:
+    ; Y, X, pat, color
+    db (192/2)-16, (256/2)-16, 1*4, 4
+    db (192/2)-16, (256/2)-16, 2*4, 15
+.size: equ $ - SPRITE_ATT_TABLE_FRAME_1
